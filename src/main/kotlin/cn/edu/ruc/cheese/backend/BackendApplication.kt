2d1b9764208c627507775ffa9e2937c76ac199f2
nictheboy <nictheboy@outlook.com>
2024-08-17 22:47:14 +0800

    chore: add code formatter (spotless) to keep code tidy

--- src/main/kotlin/cn/edu/ruc/cheese/backend/BackendApplication.kt
+++ src/main/kotlin/cn/edu/ruc/cheese/backend/BackendApplication.kt
@@ -3,9 +3,8 @@ package cn.edu.ruc.cheese.backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class BackendApplication
+@SpringBootApplication class BackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<BackendApplication>(*args)
+    runApplication<BackendApplication>(*args)
 }
