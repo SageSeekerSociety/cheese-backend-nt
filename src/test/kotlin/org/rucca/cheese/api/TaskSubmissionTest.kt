60d9b6d8e2ce693be52060e449dd281c8af0c287
nameisyui <110045515+nameisyui@users.noreply.github.com>
2024-10-31 20:53:06 +0800

    refactor(task): use enum type for approve status

--- src/test/kotlin/org/rucca/cheese/api/TaskSubmissionTest.kt
+++ src/test/kotlin/org/rucca/cheese/api/TaskSubmissionTest.kt
@@ -262,14 +262,16 @@ constructor(
                 .content(
                     """
                 {
-                  "approved": true
+                  "approved": "APPROVED"
                 }
             """
                 )
         mockMvc
             .perform(request)
             .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.data.participant.approved").value(true))
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.data.participant.approved").value("APPROVED")
+            )
     }
 
     @Test
@@ -341,6 +343,7 @@ constructor(
     @Order(40)
     fun testUpdateTaskWithFullRequest() {
         val taskId = taskIds[0]
+        print(creatorToken)
         val request =
             MockMvcRequestBuilders.patch("/tasks/$taskId")
                 .header("Authorization", "Bearer $creatorToken")
@@ -368,7 +371,7 @@ constructor(
     }
 
     @Test
-    @Order(126)
+    @Order(100)
     fun testAddTestParticipantUser2() {
         val request =
             MockMvcRequestBuilders.post("/tasks/${taskIds[0]}/participants")
@@ -382,7 +385,7 @@ constructor(
     }
 
     @Test
-    @Order(127)
+    @Order(105)
     fun testAddTestParticipantUser3() {
         val request =
             MockMvcRequestBuilders.post("/tasks/${taskIds[0]}/participants")
@@ -396,7 +399,7 @@ constructor(
     }
 
     @Test
-    @Order(127)
+    @Order(110)
     fun testAddTestParticipantTeam2() {
         val request =
             MockMvcRequestBuilders.post("/tasks/${taskIds[1]}/participants")
@@ -410,7 +413,7 @@ constructor(
     }
 
     @Test
-    @Order(127)
+    @Order(115)
     fun testSubmitTaskUserPermissionDeniedError() {
         val taskId = taskIds[0]
         val request =
@@ -431,7 +434,7 @@ constructor(
     }
 
     @Test
-    @Order(128)
+    @Order(120)
     fun testApproveTaskParticipantUser2() {
         val request =
             MockMvcRequestBuilders.patch("/tasks/${taskIds[0]}/participants")
@@ -454,7 +457,7 @@ constructor(
     }
 
     @Test
-    @Order(128)
+    @Order(125)
     fun testApproveTaskParticipantUser3() {
         val request =
             MockMvcRequestBuilders.patch("/tasks/${taskIds[0]}/participants")
