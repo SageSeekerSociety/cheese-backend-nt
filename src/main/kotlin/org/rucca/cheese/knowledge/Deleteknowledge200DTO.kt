5fca50be4796e235d092c72f25a8e5625d0109cc
Ziv <3113128547@qq.com>
2025-02-20 15:07:23 +0800

    deleteknowledgeDTO

--- src/main/kotlin/org/rucca/cheese/knowledge/Deleteknowledge200DTO.kt
+++ src/main/kotlin/org/rucca/cheese/knowledge/Deleteknowledge200DTO.kt
@@ -6,10 +6,14 @@ import io.swagger.v3.oas.annotations.media.Schema
  * @param message
  */
 data class DeleteKnowledge200ResponseDTO(
-    @Schema(example = "200", required = true, description = "状态码,200表示删除成功")
+    @Schema(example = "", required = true, description = "状态码")
     @get:JsonProperty("code", required = true)
     val code: kotlin.Int,
-    @Schema(example = "Knowledge item deleted successfully", required = true, description = "删除操作的消息")
+    @Schema(
+        example = "Knowledge item deleted successfully",
+        required = true,
+        description = "删除操作的消息",
+    )
     @get:JsonProperty("message", required = true)
     val message: kotlin.String,
-)
+){}
