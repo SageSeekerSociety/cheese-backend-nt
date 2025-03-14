cf78709e474acf660175c7e31ed2c7cc551fc763
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2025-03-14 16:41:00 +0800

    fix: not safe type convert

--- src/main/kotlin/org/rucca/cheese/auth/context/ContextKeys.kt
+++ src/main/kotlin/org/rucca/cheese/auth/context/ContextKeys.kt
@@ -1,5 +1,6 @@
 package org.rucca.cheese.auth.context
 
+import kotlin.reflect.KClass
 import org.rucca.cheese.common.persistent.IdType
 
 /**
@@ -9,18 +10,26 @@ import org.rucca.cheese.common.persistent.IdType
  * @param T The type of the value associated with this key
  * @param name The string name of the key
  */
-class ContextKey<T : Any>(val name: String) {
+class ContextKey<T : Any>(val name: String, private val clazz: KClass<T>) {
     /** Gets the typed value from a context map. */
-    @Suppress("UNCHECKED_CAST") fun get(context: Map<String, Any>): T? = context[name] as? T
+    @Suppress("UNCHECKED_CAST")
+    fun get(context: Map<String, Any>): T? {
+        val value = context[name]
+        return if (clazz.isInstance(value)) value as? T else null
+    }
 
     /** Puts a typed value into a context map. */
     fun put(context: MutableMap<String, Any>, value: T) {
         context[name] = value as Any
     }
+
+    companion object {
+        inline fun <reified T : Any> of(name: String) = ContextKey<T>(name, T::class)
+    }
 }
 
 object DomainContextKeys {
-    val DOMAIN_NAME = ContextKey<String>("domainName")
-    val RESOURCE_TYPE = ContextKey<String>("resourceType")
-    val RESOURCE_ID = ContextKey<IdType>("resourceId")
+    val DOMAIN_NAME = ContextKey.of<String>("domainName")
+    val RESOURCE_TYPE = ContextKey.of<String>("resourceType")
+    val RESOURCE_ID = ContextKey.of<IdType>("resourceId")
 }
